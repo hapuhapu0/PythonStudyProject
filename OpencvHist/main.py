a524008c30ee712c72fa6177db582b09bbe762c7
hapuhapu0 <tmdtn3624@naver.com>
2024-02-06 20:56:05 +0900

    동일 프레임에서 이미지 잘라서 오차값 비교로 수정

--- OpencvHist/main.py
+++ OpencvHist/main.py
@@ -12,23 +12,27 @@ def captureScreen(Pos: tuple) -> np.ndarray:
 def getHist(PosList: tuple) -> list:
     histList = list()
     Index = 1
+
+    Screenshot = captureScreen((0, 0, 1920, 1080))
     for Pos in PosList:
-        Image = captureScreen(Pos)
-        # Image = cv2.resize(Image, (300, 300))
-        # Image = cv2.cvtColor(Image, cv2.COLOR_BGR2GRAY)
-        cv2.imshow(str(Index), Image)
-        Index = Index + 1
+        Image = Screenshot[Pos[1]: Pos[1] + Pos[3] - Pos[1], Pos[0]: Pos[0] + Pos[2] - Pos[0]]
+        Image = cv2.resize(Image, (300, 300))
+        Image = cv2.cvtColor(Image, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow(str(Index), Image)
+
         hist = cv2.calcHist([Image], [0], None, [256], [0, 256])
+        hist = cv2.normalize(hist, hist)
         histList.append(hist)
-    cv2.waitKey(0)
+
+        Index = Index + 1
+    # cv2.waitKey(0)
 
     MainHist = histList.pop(0)
     ResultList = list()
     Index = 2
 
     for Image in histList:
-        Result = cv2.compareHist(MainHist, Image, cv2.HISTCMP_CHISQR)
-        # Result = cv2.compareHist(MainHist, Image, cv2.HISTCMP_CORREL)
+        Result = cv2.compareHist(MainHist, Image, cv2.HISTCMP_CORREL)
         ResultList.append({
             Index: Result
         })
@@ -38,15 +42,17 @@ def getHist(PosList: tuple) -> list:
         for j in range(i + 1, len(ResultList)):
             if list(ResultList[i].values())[0] > list(ResultList[j].values())[0]:
                 ResultList[i], ResultList[j] = ResultList[j], ResultList[i]
-    print(ResultList)
+
+    keys_to_print = [list(v.keys())[0] for v in ResultList[:2]]
+    print(":".join(map(str, keys_to_print)))
 
 PosList = [
-    (266, 176, 386, 339),
-    (413, 176, 532, 338),
-    (560, 176, 678, 338),
-    (266, 367, 386, 530),
-    (412, 368, 532, 530),
-    (559, 369, 678, 530)
+    (275, 186, 377, 331),
+    (420, 189, 527, 331),
+    (566, 189, 671, 333),
+    (272, 379, 380, 524),
+    (420, 379, 525, 521),
+    (568, 379, 673, 516)
 ]
 
 getHist(PosList)
